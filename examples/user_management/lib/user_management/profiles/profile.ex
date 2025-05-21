cca66a2d6734a144cc5cde5949df7d7f297131f9
Zoey Pessanha <zoey.spessanha@zeetech.io>
2025-05-20 22:47:10 -0300

    Add profiles context and schema

---
+++ examples/user_management/lib/user_management/profiles/profile.ex
@@ -0,0 +1,36 @@
+defmodule UserManagement.Profiles.Profile do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "profiles" do
+    field :user_id, :binary_id
+    field :username, :string
+    field :website, :string
+    field :avatar_url, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(profile, attrs) do
+    profile
+    |> cast(attrs, [:user_id, :username, :website, :avatar_url])
+    |> validate_required([:user_id])
+    |> unique_constraint(:user_id)
+    |> unique_constraint(:username)
+    |> validate_format(:website, ~r/^https?:\/\//, message: "must start with http:// or https://")
+  end
+
+  @doc """
+  Changeset for updating a profile.
+  Only allows updating certain fields, not the user_id.
+  """
+  def update_changeset(profile, attrs) do
+    profile
+    |> cast(attrs, [:username, :website, :avatar_url])
+    |> unique_constraint(:username)
+    |> validate_format(:website, ~r/^https?:\/\//, message: "must start with http:// or https://")
+  end
+end
