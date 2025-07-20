e8da3d72832e75832f4c13bf90175cd47c32c95f
Zoey Pessanha <zoey.spessanha@zeetech.io>
2025-07-20 11:00:29 -0300

    chore: add 1.16 elixir version to ci

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Supabase.MixProject do
     [
       app: :supabase_potion,
       version: @version,
-      elixir: "~> 1.14",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
