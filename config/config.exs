b4d6464647db11e33e6eda739a95ad05f75df894
Zoey Pessanha <zoey.spessanha@zeetech.io>
2025-10-04 19:13:04 -0300

    fix: well, JSON only on v1.18

--- config/config.exs
+++ config/config.exs
@@ -5,8 +5,10 @@ if config_env() == :dev do
     base_url: System.fetch_env!("SUPABASE_URL"),
     api_key: System.fetch_env!("SUPABASE_KEY"),
     env: config_env()
+
+  config :supabase_potion, json_library: JSON
 end
 
-if config_env() in [:dev, :test] do
-  config :supabase_potion, json_library: JSON
+if config_env() == :test do
+  config :supabase_potion, json_library: Jason
 end
